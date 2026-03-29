ea01ac5732e2caec1367c381cc7b223ace305495
vishal bhelave <vishalbhelave1602@gamil.com>
2026-03-29 08:00:56 +0530

    Day 1: Added null datatype example

---
+++ index.js
@@ -0,0 +1,36 @@
+console.log([] + []);
+console.log([] + {});
+console.log({} + []);
+
+
+//   primitive  DATRA TYPE 
+
+// 1. NUMBER
+let  NUMB= 233;
+
+console.log(NUMB)
+
+// 2. BIGINT 
+
+let bigint=33333333333333333333333333333;
+console.log(bigint);
+
+// 3. str 
+
+let str= "vishal";
+console.log(str);
+
+//4. undefine
+
+let a ;
+
+console.log(a);
+
+// 5. NULL 
+
+let balance= null;
+
+console.log(null);
+
+
+//  6. git 
