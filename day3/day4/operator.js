98c73b60aecd350f53cf13d8febcf966465d3ba0
vishal bhelave <vishalbhelave1602@gamil.com>
2026-03-31 06:53:19 +0530

    day3 operater advance

---
+++ day3/day4/operator.js
@@ -0,0 +1,92 @@
+//  task is dsa  42 and 43 withj collage study with notes of dcn 
+//  practicval 
+//  sl project 
+//  oos project
+
+
+// topic is comaprison operster 
+
+// nukber  to number
+
+// let a1=1;
+// let a2=2;
+
+// console.log(a1===a2);
+
+
+//equal to  
+
+// let num=10;
+// let str="10"
+// console.log(num==str);
+//  type conversion hoga string to number me 
+
+
+
+//  strict equal to 
+
+// let num=10;
+// // let str="ten"
+// console.log(num===str);
+// //  type conversion hoga string ka number me 
+
+// //  === first check the type then compare the value 
+
+// console.log(num===str);
+
+//  rule  bana ya 
+
+//  null == undefine // true 
+
+// null===  undefine  // false 
+
+//  undefine comparision
+console.log(null===undefined)
+
+console.log(null==undefined)
+//  null 
+// console.log(null==0); null sirf undefine ke equal hoga 
+console.log(null<0);
+console.log(null>0);
+console.log(null<=0);
+
+
+//  undefine comparison 
+
+console.log(undefined<0);
+console.log(undefined==0);
+console.log(undefined<=0);
+console.log(undefined>=0);
+console.log(NaN==NaN)
+let str3="vishal";
+let str4="mohit";
+console.log(str3==str4)
+
+//  
+let ab=123;
+let ab1="123";
+let ab2=123;
+
+console.log(ab==ab1)
+
+console.log(ab==ab1==ab2)
+
+// &&  operater 
+
+let age=33;
+let vishal=33;
+console.log(age>22&&vishal>20);
+
+//  ! operater 
+
+
+
+
+//  bitwise operater 
+console.log(4&5);
+console.log(5&4)
+console.log(3&4)
+// 011
+// 100
+
+console.log(5^4);
