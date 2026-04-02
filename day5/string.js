cebd244d18eec63edb0c3ebbab7de0440690c5aa
vishal bhelave <vishalbhelave1602@gmail.com>
2026-04-02 07:39:25 +0530

    Added comparison operators

---
+++ day5/string.js
@@ -0,0 +1,78 @@
+//  pass by value and pass by refe=res=nces 
+
+// let  value change kr sakte ho 
+//  const vo value final hai 
+1
+
+let str="krishna";
+console.log(str)
+
+let price=59;
+console.log(`price of the tomoto is ${price}`);
+
+//  string concatinaton 
+
+let s1= " hello";
+let s2= " coder army";
+
+console.log(s1+s2);
+console.log(s1.length)
+
+//  quetation ko bhi print karana hai 
+
+console.log(`"hello coders army "`)
+//  hello 
+
+
+
+console.log( " vishal \\n bhelave")
+// \ n  ko bhi print karn hia 
+console.log( " vishal \n bhelave")
+
+
+let special = " rohit ";
+console.log("special")
+console.log(special)
+console.log(special[1]);
+//  char 
+console.log(special.charAt(3));
+console.log(special.charAt[2]);
+
+console.log(special.toLocaleLowerCase());
+console.log(special.toLocaleUpperCase());
+
+//   searching in string 
+
+
+let hero="hellow coder"
+
+//  indexof give the first index of sub string 
+console.log(hero.indexOf("coder"))
+console.log(hero.lastIndexOf('coder'))
+
+// extracting sub string 
+
+//  last vala index include nhi rahega 
+let newString="helloDON";
+console.log(newString.slice(-7,-1));
+//  slice can take negative index also 
+
+//  sub string me nahi kr sakltye 
+console.log(newString.substring(0,3));
+
+
+//  replace 
+
+let str11=" hello ! bhiya !kaise ! ho ";
+console.log(str11.replace("bhiya","money"))
+console.log(str11.split("!"))
+//  trim 
+
+let str12=" hello bhiya ji "
+
+console.log(str12.trim())
+console.log(lasteststring)
+
+
+
+
