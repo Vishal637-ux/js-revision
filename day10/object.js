62307e169b11e1d2c0bd763cc72c8f62f29a4e9a
vishal bhelave <vishalbhelave1602@gmail.com>
2026-04-07 21:13:04 +0530

    OBJECTS

---
+++ day10/object.js
@@ -0,0 +1,24 @@
+
+
+//  const 
+//  assign operater 
+const obj1={a1:1,b:2};
+const obj2={c:3,d:4};
+
+ obj3=Object.assign( {},obj1,obj2);
+
+console.log(obj3,obj2,obj1)
+
+// 
+obj3.a=10;
+console.log(obj3)
+
+// spread operater 
+
+const obj5={...obj1,...obj2,...obj3}
+console.log(obj5)
+
+//  advantage of spread operater and asssign operater 
+//  class 
+
+//  how to revise lap top kholo and  practice karo 
