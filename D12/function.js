222596466733333a72c67b349374d8e3c9831547
vishal bhelave <vishalbhelave1602@gmail.com>
2026-04-11 21:26:15 +0530

    OBJECTS

---
+++ D12/function.js
@@ -0,0 +1,118 @@
+// object prototype   object nichhe vale ke prototype nahi kr sakta 
+// prototype chaining isse hi kahte hai 
+// object heap me store hota hai 
+//  ghashing tequenic ka use karta hai 
+// referance 
+// function greet() {
+//    console.log("HARI");
+// }
+
+// greet();
+// function greet() {
+//    console.log(" hari hari");
+// }
+
+// for(let i = 1; i <= 108; i++) {
+//    greet();
+// }
+
+
+//  function greet(){
+//     console.log(" hare krishna hare krishna")
+//  }
+
+//  for(let i=1; i<=10; i++){
+
+//     greet();
+//  }
+
+//  2. Function with Parameters
+// function greet(name) {
+//    console.log("Hare " + name);
+// }
+
+// greet("Vishal");
+// greet("Rahul");
+
+
+// 3. Return Value
+// Function can give result back using return
+
+// function add(a, b) {
+//    return a + b;
+// }
+
+// let result = add(5, 3);
+// console.log(result);
+
+// 4. Function Expression
+// Store function in variable
+// const sayHi = function() {
+//    console.log("Hi");
+// };
+
+// sayHi();
+
+
+// // 5. Arrow Function (Modern JS)
+// const add = (a, b) => a + b;
+
+// console.log(add(2, 3));
+
+// function hi(name){
+//     console.log(name)
+// } // parameter
+// hi("Vishal")      // argument
+
+
+//1. Make function to square a number
+
+//  function square(num){
+
+//     console.log(num*num)
+//  }
+
+
+// function square(num) {
+//     console.log(num * num);
+// }
+
+// const readline = require("readline");
+
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
+
+// rl.question("Give me number: ", function(answer) {
+//     square(Number(answer));
+//     rl.close();
+// });
+
+
+// 2. Make function to check even/odd
+
+ function check(num){
+if (num%2==0){
+    console.log("even")
+}
+else{
+    console.log( "odd");
+}
+ }
+
+ check(3);
+ check(2)
+
+
+//  3.Make function to reverse string
+
+ function reverse(){
+ 
+         
+
+
+ 
+ }
+
+ reverse("vishal")
